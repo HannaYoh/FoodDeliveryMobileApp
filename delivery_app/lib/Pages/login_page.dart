f2c192b8f779be75484856eb5dff814cc95ec7b4
Hanna Yohannes <hannayohmek@gmail.com>
2024-11-25 22:09:43 +0300

    Themes added

---
+++ delivery_app/lib/Pages/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            //message
+            const Text('Food delivery app')
+          ],
+        ),
+      ),
+    );
+  }
+}
